0fdf4957adf8cd5e1f314168f2c08dfbb5dcdc35
Jaxon Schraut <jqschraut@gmail.com>
2022-09-06 08:00:58 -0500

    Initial Commit

---
+++ Mod2CreativeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod2CreativeApp
+//
+//  Created by JAXON SCHRAUT on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
