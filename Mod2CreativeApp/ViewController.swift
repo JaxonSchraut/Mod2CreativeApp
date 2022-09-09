be9e71a062ce55c7e6e98b59d820f7d358a959d7
Jaxon Schraut <jqschraut@gmail.com>
2022-09-09 07:37:21 -0500

    Initial Commit

--- Mod2CreativeApp/ViewController.swift
+++ Mod2CreativeApp/ViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var numberOne: UITextField!
+    @IBOutlet weak var numberTwo: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        var num1 = 0
+        var num2 = 0
+        if Int(numberOne.text!) != nil{
+            num1 = Int(numberOne.text!)!
+        }
+        if Int(numberTwo.text!) != nil{
+            num2 = Int(numberTwo.text!)!
+        }
+        let total = num1+num2
+        answerLabel.text = String(total)
+        numberOne.resignFirstResponder()
+        numberTwo.resignFirstResponder()
+    }
+    
+    @IBAction func modButton(_ sender: UIButton) {
+        var num1 = 0
+        var num2 = 0
+        if Int(numberOne.text!) != nil{
+            num1 = Int(numberOne.text!)!
+        }
+        if Int(numberTwo.text!) != nil{
+            num2 = Int(numberTwo.text!)!
+        }
+        var total = 0
+        if num2 != 0 {
+            total = Int(num1 % num2)
+            answerLabel.text = String(total)
+        } else {
+            answerLabel.text = "Error"
+        }
+        
+        numberOne.resignFirstResponder()
+        numberTwo.resignFirstResponder()
+    }
+    
+    @IBAction func pythagoreanButton(_ sender: UIButton){
+        var num1 = 0.0
+        var num2 = 0.0
+        if Int(numberOne.text!) != nil{
+            num1 = Double(numberOne.text!)!
+        }
+        if Int(numberTwo.text!) != nil{
+            num2 = Double(numberTwo.text!)!
+        }
+        let total = sqrt((num1*num1)+(num2*num2))
+        answerLabel.text = String(total)
+        numberOne.resignFirstResponder()
+        numberTwo.resignFirstResponder()
+    }
+    
 }
 
